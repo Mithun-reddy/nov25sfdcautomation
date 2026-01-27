a12d3dfb8a34dcedbc916168c4c623171b500b33
Mithun-reddy <mithun.doc@gmail.com>
2026-01-27 23:25:51 +0530

    opp branch commit

--- src/main/java/demo/Entry.java
+++ src/main/java/demo/Entry.java
@@ -4,6 +4,7 @@ public class Entry {
 	public static void main(String[] args) {
 		
 		System.out.println(System.getProperty("user.dir")+"/src/main/resources/screenshots/"+System.currentTimeMillis()+".png");
+		System.out.println();
 	}
 
 }
