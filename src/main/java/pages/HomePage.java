3ab8dd38b7bc210cb22dc14172342cbc799c7cbf
Mithun-reddy <mithun.doc@gmail.com>
2026-01-28 23:20:36 +0530

    added class file

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,9 +1,12 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HomePage extends BasePage{
 
+	public WebElement usermenu;
+	
 	public HomePage(WebDriver driver) {
 		super(driver);
 	}
