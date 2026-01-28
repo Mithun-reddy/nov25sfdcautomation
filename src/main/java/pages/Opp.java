b5f9f962dfcfcbda41167d35023e70f3b68b69ab
Mithun-reddy <mithun.doc@gmail.com>
2026-01-28 23:20:36 +0530

    added class file

---
+++ src/main/java/pages/Opp.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class Opp extends BasePage {
+
+	public Opp(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
